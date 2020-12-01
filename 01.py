5b46809b67976173d7497d8d9c98ed16c699734b
marin1401 <58388551+marin1401@users.noreply.github.com>
2020-12-01 10:21:24 +0100

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,27 @@
+#Day 01
+
+with open('./01.txt') as myinput:
+    inputlines = myinput.readlines()
+
+#Part 1
+solution = False
+for i in inputlines:
+    for j in inputlines:
+        if int(i)+int(j) == 2020:
+            print(int(i)*int(j))
+            solution = True
+            break
+    if solution == True:
+        break
+
+#Part 2
+solution = False
+for i in inputlines:
+    for j in inputlines:
+        for k in inputlines:
+            if int(i)+int(j)+int(k) == 2020:
+                print(int(i)*int(j)*int(k))
+                solution = True
+                break
+        if solution == True:
+            break
