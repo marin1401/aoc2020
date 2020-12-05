c9a9a7b5f7da7a0949548131ffb8194d21e0ebe9
marin1401 <58388551+marin1401@users.noreply.github.com>
2020-12-05 10:33:04 +0100

    Update 04.py

--- 04.py
+++ 04.py
@@ -3,7 +3,6 @@
 with open('./04.txt') as myinput:
     inputlines = myinput.readlines()
 
-
 inputlines = ''.join(line.replace(' ','\n') for line in inputlines).splitlines()
 
 passports = []
@@ -17,15 +16,58 @@ for line in inputlines:
         passport_data = {}
 passports.append(passport_data)
 
+
+#Part 1
+
 passport_required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 
 counter = 0
+valid_passports = []
 for passport in passports:
     if all(keys in passport.keys() for keys in passport_required_fields):
         counter += 1
+        valid_passports.append(passport)
 
-#Part 1
 print(counter)
 
 #Part 2
-#I refuse to do this because there are only 2718281828459045 conditions to check and imo there should be more
+
+invalid_chars = 'ghijklmnopqrstuvwxyz'
+valid_ecl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+for passport in valid_passports:
+    for key, value in passport.items():
+        if key == 'byr':
+            if int(value) < 1920 or int(value) > 2002:
+                passport[key] = 'invalid'
+        if key == 'iyr':
+            if int(value) < 2010 or int(value) > 2020:
+                passport[key] = 'invalid'
+        if key == 'eyr':
+            if int(value) < 2020 or int(value) > 2030:
+                passport[key] = 'invalid'
+        if key == 'hgt':
+            if value[-2:] == 'cm':
+                if int(value[:-2]) < 150 or int(value[:-2]) > 193:
+                    passport[key] = 'invalid'
+            elif value[-2:] == 'in':
+                if int(value[:-2]) < 59 or int(value[:-2]) > 76:
+                    passport[key] = 'invalid'
+            else:
+                passport[key] = 'invalid'
+        if key == 'hcl':
+            if value[0] != '#' or len(value) != 7 or any(chars in invalid_chars for chars in value[1:]):
+                passport[key] = 'invalid'
+        if key == 'ecl':
+            if value not in valid_ecl:
+                passport[key] = 'invalid'
+        if key == 'pid':
+            if len(value) != 9:
+                passport[key] = 'invalid'
+
+counter = 0
+for passport in valid_passports:
+    if 'invalid' not in passport.values():
+        counter += 1
+
+print(counter)
