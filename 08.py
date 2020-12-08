b4b1ce019c9a00ce229bdc61016b55504d122e31
marin1401 <58388551+marin1401@users.noreply.github.com>
2020-12-08 11:04:46 +0100

    Update 08.py

--- 08.py
+++ 08.py
@@ -9,23 +9,42 @@ for line in inputlines:
     operation.append(line.split()[0])
     argument.append(int(line.split()[1]))
 
-accumulator = 0
-i = 0
-been_there_done_that = set()
-while i < len(inputlines):
-    if i in been_there_done_that:
-        break
-    if operation[i] == 'nop':
-        been_there_done_that.add(i)
-        i += 1
-    if operation[i] == 'acc':
-        been_there_done_that.add(i)
-        accumulator += argument[i]
-        i += 1
-    if operation[i] == 'jmp':
-        been_there_done_that.add(i)
-        i += argument[i]
+def get_acc(operation, argument):
+    been_there_done_that = set()
+    accumulator = 0
+    i = 0
+    while i < len(operation):
+        if i in been_there_done_that:
+            break
+        if operation[i] == 'nop':
+            been_there_done_that.add(i)
+            i += 1
+        if operation[i] == 'acc':
+            been_there_done_that.add(i)
+            accumulator += argument[i]
+            i += 1
+            if len(operation) - 1 <= i:
+                break
+        if operation[i] == 'jmp':
+            been_there_done_that.add(i)
+            i += argument[i]
+    return [accumulator, i]
 
 #Part 1
 
-print(accumulator)
+print(get_acc(operation, argument)[0])
+
+#Part 2
+
+ops = [['jmp', 'nop'], ['nop', 'jmp']]
+
+for line in ops:
+    for op in range(len(operation)):
+        if operation[op] == line[0]:
+            operation[op] = line[1]
+            sol = get_acc(operation, argument)
+            if sol[1] == len(operation) - 1:
+                result = sol[0]
+            operation[op] = line[0]
+
+print(result)
