68ae712e6db4a888823edb6c1cfd6f84a88d90a9
marin1401 <58388551+marin1401@users.noreply.github.com>
2020-12-11 17:57:44 +0100

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,43 @@
+#Day 10
+
+with open('./10.txt') as myinput:
+    inputlines = myinput.readlines()
+
+num_list = sorted([int(line) for line in inputlines])
+num_list.insert(0, 0)
+num_list.append(num_list[-1] + 3)
+num_list_size = len(num_list)
+
+#Part 1
+
+count_diff_1 = 0
+count_diff_3 = 0
+for i in range(num_list_size - 1):
+    diff = num_list[i+1] - num_list[i]
+    if diff == 1:
+        count_diff_1 += 1
+    if diff == 3:
+        count_diff_3 += 1
+
+print(count_diff_1 * count_diff_3)
+
+#Part 2
+
+# NOTE:
+# For this part I exploited the fact that there aren't more than 5 consecutive adapters
+# and that there are always 2 jolts in between consecutive adapter groups
+
+import more_itertools as mit
+
+groups = [list(group) for group in mit.consecutive_groups(num_list)]
+
+arrangements = 1
+for group in groups:
+    if len(group) == 3:
+        arrangements = arrangements * 2
+    elif len(group) == 4:
+        arrangements = arrangements * 4
+    elif len(group) == 5:
+        arrangements = arrangements * 7
+
+print(arrangements)
