6da85b6205e4c24275d1e0177da7189c18c0521a
marin1401 <58388551+marin1401@users.noreply.github.com>
2020-12-29 19:29:23 +0100

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,42 +1,22 @@
-#Day 02
+#Day 2
 
 with open('./02.txt') as myinput:
     inputlines = myinput.readlines()
 
+inputlines = [i.split() for i in inputlines]
+
+policy_1_counter, policy_2_counter = 0, 0
+for line in inputlines:
+    num_1, num_2 = map(int, line[0].split('-'))
+    if num_1 <= line[2].count(line[1][0]) <= num_2:
+        policy_1_counter += 1
+    if (line[2][num_1-1] == line[1][0]) != (line[2][num_2-1] == line[1][0]):
+        policy_2_counter += 1
+
 #Part 1
-    
-min_number_appearance = []
-max_number_appearance = []
-letter_to_appear = []
-password = []
 
-for i in range(len(inputlines)):
-    min_number_appearance.append(int(inputlines[i].split()[0].split('-')[0]))
-    max_number_appearance.append(int(inputlines[i].split()[0].split('-')[1]))
-    letter_to_appear.append(inputlines[i].split()[1][0])
-    password.append(inputlines[i].split()[2])
-
-counter = 0
-for i in range(len(inputlines)):    
-    if min_number_appearance[i] <= password[i].count(letter_to_appear[i]) <= max_number_appearance[i]:
-        counter += 1
-print(counter)
+print(policy_1_counter)
 
 #Part 2
 
-position_1 = min_number_appearance
-position_2 = max_number_appearance
-letter_on_position_1 = []
-letter_on_position_2 = []
-
-for i in range(len(inputlines)):   
-    letter_on_position_1.append(password[i][position_1[i]-1])
-    letter_on_position_2.append(password[i][position_2[i]-1])
-
-letters_to_check = [i + j for i, j in zip(letter_on_position_1, letter_on_position_2)]
-
-counter = 0
-for i in range(len(inputlines)):
-    if letters_to_check[i].count(letter_to_appear[i]) == 1:
-        counter += 1
-print(counter)
+print(policy_2_counter)
