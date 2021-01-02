a6cf8f20531597128f4d86dfb5105c42e7ee08f3
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-02 18:47:46 +0100

    Update 03.py

--- 03.py
+++ 03.py
@@ -3,25 +3,24 @@
 with open('./03.txt') as myinput:
     inputlines = myinput.readlines()
 
-inputlines = [line.replace('.', '0').replace('#', '1').replace('\n', '') for line in inputlines]
-toboggan = [list(map(int, i)) for i in inputlines]
+toboggan = [line.strip() for line in inputlines]
 
 slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 trees = []
 trees_product = 1
 
 for slope in slopes:
-    position = 0
-    counter = 0
-    if slope[1] > 1:
-        del toboggan[1::slope[1]] #this is wrong, but simple
+    position, counter = 0, 0
     for row in toboggan:
+        if slope[1] > 1:
+            if toboggan.index(row) % slope[1] != 0:
+                continue
         if position < len(row) - slope[0]:
-            if row[position] == 1:
+            if row[position] == '#':
                 counter += 1
         for i in range(slope[0] + 1):
             if position == len(row) - i:
-                if row[position] == 1:
+                if row[position] == '#':
                     counter += 1
                 position = -i
         position += slope[0]
@@ -29,7 +28,9 @@ for slope in slopes:
     trees_product = trees_product * counter
 
 #Part 1
+
 print(trees[1])
 
 #Part 2
+
 print(trees_product)
