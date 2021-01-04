b1a1a442fe87455157d9f8c7977c741ba8decc70
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-04 20:35:35 +0100

    Update 04.py

--- 04.py
+++ 04.py
@@ -4,70 +4,39 @@ with open('./04.txt') as myinput:
     inputlines = myinput.readlines()
 
 inputlines = ''.join(line.replace(' ','\n') for line in inputlines).splitlines()
-
-passports = []
-passport_data = {}
-for line in inputlines:
-    if len(line) > 0:
-        key, val = line.split(':')
-        passport_data[key] = val
-    elif len(line) == 0:
-        passports.append(passport_data)
-        passport_data = {}
-passports.append(passport_data)
-
+inputlines.append('')
 
 #Part 1
 
 passport_required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 
-counter = 0
-valid_passports = []
-for passport in passports:
-    if all(keys in passport.keys() for keys in passport_required_fields):
-        counter += 1
-        valid_passports.append(passport)
+passports = []
+passport = {}
+for line in inputlines:
+    if line:
+        k, v = line.split(':')
+        passport[k] = v
+    else:
+        if all(k in passport.keys() for k in passport_required_fields):
+            passports.append(passport)
+        passport = {}
 
-print(counter)
+print(len(passports))
 
 #Part 2
 
-invalid_chars = 'ghijklmnopqrstuvwxyz'
+valid_chars = '0123456789abcdef'
 valid_ecl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
-for passport in valid_passports:
-    for key, value in passport.items():
-        if key == 'byr':
-            if int(value) < 1920 or int(value) > 2002:
-                passport[key] = 'invalid'
-        if key == 'iyr':
-            if int(value) < 2010 or int(value) > 2020:
-                passport[key] = 'invalid'
-        if key == 'eyr':
-            if int(value) < 2020 or int(value) > 2030:
-                passport[key] = 'invalid'
-        if key == 'hgt':
-            if value[-2:] == 'cm':
-                if int(value[:-2]) < 150 or int(value[:-2]) > 193:
-                    passport[key] = 'invalid'
-            elif value[-2:] == 'in':
-                if int(value[:-2]) < 59 or int(value[:-2]) > 76:
-                    passport[key] = 'invalid'
-            else:
-                passport[key] = 'invalid'
-        if key == 'hcl':
-            if value[0] != '#' or len(value) != 7 or any(chars in invalid_chars for chars in value[1:]):
-                passport[key] = 'invalid'
-        if key == 'ecl':
-            if value not in valid_ecl:
-                passport[key] = 'invalid'
-        if key == 'pid':
-            if len(value) != 9:
-                passport[key] = 'invalid'
-
 counter = 0
-for passport in valid_passports:
-    if 'invalid' not in passport.values():
-        counter += 1
+for passport in passports:
+    if int(passport['byr']) in range(1919, 2003):
+        if int(passport['iyr']) in range(2009, 2021):
+            if int(passport['eyr']) in range(2019, 2031):
+                if passport['hgt'][-2:] == 'cm' and int(passport['hgt'][:-2]) in range(149, 194) or passport['hgt'][-2:] == 'in' and int(passport['hgt'][:-2]) in range(58, 77):
+                    if passport['hcl'][0] == '#' and len(passport['hcl']) == 7 and all(chars in valid_chars for chars in passport['hcl'][1:]):
+                        if passport['ecl'] in valid_ecl:
+                            if len(passport['pid']) == 9:
+                                counter += 1
 
 print(counter)
